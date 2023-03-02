20de50b2b8fe1b5567deda29734561eed313d5f6
jarvisye <972763217@qq.com>
2023-03-02 22:55:37 +0800

    20230302: Update.

---
+++ leetcode_practice/20230302_141/solution.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode * head)
+    {
+        ListNode *fast = head, *slow = head;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    ListNode node1(1);
+    ListNode node2(2);
+    ListNode node3(3);
+    ListNode node4(4);
+    ListNode node5(5);
+
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = &node4;
+    node4.next = &node5;
+    node5.next = &node1;
+
+    printf("has cycle %d\n", Solution().hasCycle(&node1));
+    return 0;
+}
